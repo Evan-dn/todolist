a391babd7d7723d5aedcf740ca95118ded496e54
evan wichlarz <evanwichlarz1@hotmail.fr>
2020-01-28 08:29:39 -0500

    Updates

---
+++ precache-manifest.cbc26c8439f7a9a813e40da4c2eb2c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3613ee142dc270e9f8d20cd696d67b0",
+    "url": "/Evan-dn.github.io/todolist/index.html"
+  },
+  {
+    "revision": "3b974656503b530f4f19",
+    "url": "/Evan-dn.github.io/todolist/static/css/main.207d221c.chunk.css"
+  },
+  {
+    "revision": "57809a989be1227f21e6",
+    "url": "/Evan-dn.github.io/todolist/static/js/2.36bb8f0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Evan-dn.github.io/todolist/static/js/2.36bb8f0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b974656503b530f4f19",
+    "url": "/Evan-dn.github.io/todolist/static/js/main.78ee6450.chunk.js"
+  },
+  {
+    "revision": "6b20468592239f1d6ea8",
+    "url": "/Evan-dn.github.io/todolist/static/js/runtime-main.2511af4e.js"
+  }
+]);
